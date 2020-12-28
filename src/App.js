f6bcd5660252d7405e0fb2cc7d4cc18cf7d38e57
junholee <juno7803@naver.com>
2020-12-28 12:21:35 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {RecoilRoot, atom, useRecoilState} from "recoil"
 
-function App() {
+const countState = atom({
+  key: 'countState',
+  default: 0,
+});
+
+const App =()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecoilRoot>
+        <Counter/>
+      </RecoilRoot>
     </div>
   );
 }
 
 export default App;
+
+function Counter() {
+  const [count, setCount] = useRecoilState(countState);
+  const addCount = () =>{
+    setCount(count+1);
+  }
+  const subCount = () =>{
+    setCount(count-1);
+  }
+  return (
+    <div>
+      <button onClick={addCount}>up</button>
+      <button onClick={subCount}>sub</button>
+      <p>{count}</p>
+    </div>
+  )
+}
